17dd0e8ff4f621708713caaccfb9c593233c129a
Sai-shashank-2005 <saishashankpedapati@gmail.com>
2026-04-24 07:26:17 +0530

    upgraded the footer links and responsiveness , added comming soon pages for links with no pages

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ import Cybersec from "./pages/Cybersec";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 import TermsOfService from "./pages/TermsOfService";
 
+// IMPORT THE COMING SOON PAGE HERE
+import ComingSoon from "./pages/ComingSoon"; 
+
 function LandingPage() {
   return (
     <main>
@@ -27,11 +30,13 @@ function App() {
           <Route path="/contact" element={<Contact />} />
           <Route path="/cybersec" element={<Cybersec />} />
           
+          {/* ADD THE COMING SOON ROUTE HERE */}
+          <Route path="/coming-soon" element={<ComingSoon />} />
+          
+          {/* Legal Routes */}
           <Route path="/privacy" element={<PrivacyPolicy />} />
           <Route path="/terms" element={<TermsOfService />} />
-          {/* Legal Routes - Placed before catch-all */}
           
-
           {/* Catch-all: Always keep this last */}
           <Route path="*" element={<LandingPage />} />
         </Routes>
